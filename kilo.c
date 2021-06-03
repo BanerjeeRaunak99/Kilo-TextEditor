e1fe3c8ed3a4fcc02cf8b73f75ed4e344d4da455
BanerjeeRaunak99 <banerjeeraunak97@gmail.com>
2021-06-03 15:44:27 +0530

    completed the editor

--- kilo.c
+++ kilo.c
@@ -36,6 +36,8 @@ enum editorKey{
 enum editorHighlight{
 	HL_NORMAL = 0,
 	HL_COMMENT,
+	HL_KEYWORD1,
+	HL_KEYWORD2,
 	HL_STRING,
 	HL_NUMBER,
 	HL_MATCH
@@ -46,6 +48,7 @@ enum editorHighlight{
 struct editorSyntax{
 	char *filetype;
 	char **filematch;
+	char **keywords;
 	char *singleline_comment_start;
 	int flags;
 };
@@ -76,10 +79,17 @@ struct editorConfig{
 struct editorConfig E;
 /***filetypes***/
 char *C_HL_extensions[] = {".c",".h",".cpp",NULL};
+char *C_HL_keywords[] = {
+	"switch","if","while","for","break","continue","return","else","struct","union","typedef",
+	"static","enum","class","case",
+
+	"int|","long|","double|","float|","char|","unsigned|","signed|","void|",NULL
+};
 struct editorSyntax HLDB[] = {
 	{
 		"c",
 		C_HL_extensions,
+		C_HL_keywords,
 		"//",
 		HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
 	},
@@ -199,6 +209,7 @@ void editorUpdateSyntax(erow *row){
 	row->hl = realloc(row->hl,row->rsize);
 	memset(row->hl,HL_NORMAL,row->rsize);
 	if(E.syntax == NULL) return;
+	char **keywords = E.syntax->keywords;
 	char *scs = E.syntax->singleline_comment_start;
 	int scs_len = scs?strlen(scs):0;
 	int prev_sep = 1;
@@ -243,6 +254,25 @@ void editorUpdateSyntax(erow *row){
 			continue;
 		}
 		}
+		if(prev_sep){
+			int j;
+			for(j=0;keywords[j];j++){
+				int klen = strlen(keywords[j]);
+				int kw2 = keywords[j][klen-1] == '|';
+				if(kw2) klen--;
+				if(!strncmp(&row->render[i],keywords[j],klen)&&
+						is_separator(row->render[i+klen])){
+					memset(&row->hl[i],kw2?HL_KEYWORD2:HL_KEYWORD1,klen);
+					i+=klen;
+					break;
+				}
+				
+			}
+			if(keywords[j]!=NULL){
+				prev_sep=0;
+				continue;
+			}
+		}
 		prev_sep = is_separator(c);
 		i++;
 	}
@@ -253,6 +283,8 @@ int editorSyntaxToColor(int hl){
 		case HL_MATCH: return 34;
 		case HL_STRING: return 35;
 		case HL_COMMENT: return 36;
+		case HL_KEYWORD1: return 33;
+		case HL_KEYWORD2: return 32;
 		default:return 37;
 	}
 }
@@ -606,7 +638,18 @@ void editorDrawRows(struct abuf *ab){
 			int current_color = -1;
 			int j;
 			for(j=0;j<len;j++){
-				if(hl[j]==HL_NORMAL){
+				if(iscntrl(c[j])){
+					char sym = (c[j]<=26)?'@'+c[j]:'?';
+					abAppend(ab,"\x1b[7m",4);
+					abAppend(ab,&sym,1);
+					abAppend(ab,"\x1b[m",3);
+					if(current_color!=-1){
+						char buf[16];
+						int clen = snprintf(buf,sizeof(buf),"\x1b[%dm",current_color);
+						abAppend(ab,buf,clen);
+					}
+				}
+				else if(hl[j]==HL_NORMAL){
 					if(current_color != -1){
 					abAppend(ab,"\x1b[39m",5);
 					current_color = -1;

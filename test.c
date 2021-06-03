e10fffd24aece35777ecbbc40b0ce2646f1a1a54
BanerjeeRaunak99 <banerjeeraunak97@gmail.com>
2021-06-03 12:21:29 +0530

    added comment and string highlighting

--- test.c
+++ test.c
@@ -2,3 +2,6 @@ int hello 23
 are you theresaf
 sfafs sfaf 4 46 46846a4s684 a8 4a6 8 68f68a4 s68f
 fafs4 48885:
+"this is a test" sjdhbajnsoaf "escape \d" 'jhghig' bgvihgih
+   //this is a comment
+safasfasf //test
